253e1ed0813e3a0b6ffd8dc018dd75137ad6ba06
minchao19920917 <499790879@qq.com>
2018-09-10 17:14:41 +0800

    修改美化了样式

--- pages/translate/translate.js
+++ pages/translate/translate.js
@@ -1,4 +1,23 @@
+// pages/translate/translate.js
 Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
   onReady: function () {
     this.animation = wx.createAnimation()
   },
@@ -51,5 +70,47 @@ Page({
       .skew(0, 0)
       .step({ duration: 0 })
     this.setData({ animation: this.animation.export() })
+  },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
+
